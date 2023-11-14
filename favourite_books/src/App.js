63df12ed258e451f7a8b5c13dedb3650d40d95c0
Abdul-KMC <mfnadwi0522@gmail.com>
2023-11-15 01:17:09 +0530

    feat: created a child component

--- favourite_books/src/App.js
+++ favourite_books/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BookList from './BookList';
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">Favourite Books</h1>
       <p className="description">Add all your favourite books here.</p>
+      <BookList/>
     </div>
   );
 }
