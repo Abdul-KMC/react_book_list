f364192c49c88cbc25fa68f775b725973d947735
Abdul-KMC <mfnadwi0522@gmail.com>
2023-11-22 20:39:40 +0530

    feat: managing state for adding books dynamically

--- favourite_books/src/App.js
+++ favourite_books/src/App.js
@@ -1,18 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 import BookList from './components/BookList';
+import AddBookForm from './components/AddBookForm';
 
-const bookList = {
-  book1: "A Study in Scarlet & The Sign of Four",
-  book2: "The Hound of the Baskervilles & The Valley of Fear",
-  book3: "The Adventures of Sherlock Holmes"
-}
+// const bookList = {
+//   book1: "A Study in Scarlet & The Sign of Four",
+//   book2: "The Hound of the Baskervilles & The Valley of Fear",
+//   book3: "The Adventures of Sherlock Holmes"
+// }
 
 function App() {
+
+  const [bookList, setBookList] = useState(["A Study in Scarlet & The Sign of Four", "The Hound of the Baskervilles & The Valley of Fear", "The Adventures of Sherlock Holmes"])
+
+  const addBook = (newBook) => {
+    setBookList([...bookList, newBook]);
+}
+
   return (
     <div className="App">
-      <h1 className="title">Favourite Books</h1>
-      <h3 className="description">Add all your favourite books here.</h3>
-      <BookList list={bookList}  />
+      <h1 className="title">Favorite Books</h1>
+      <h3 className="description">Add all your favorite books here.</h3>
+      <AddBookForm updateList={addBook}/> 
+      <BookList list={bookList}/>
     </div>
   );
 }
