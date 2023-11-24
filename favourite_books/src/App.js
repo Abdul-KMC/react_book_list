e01b59dd3fcdc1e380eae7c4340c7ff6218daf64
Abdul-KMC <mfnadwi0522@gmail.com>
2023-11-24 20:21:56 +0530

    feat: added POST, GET and DELETE request for mockAPI

--- favourite_books/src/App.js
+++ favourite_books/src/App.js
@@ -1,33 +1,75 @@
-import React, { useState } from 'react';
+// App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import BookList from './components/BookList';
 import AddBookForm from './components/AddBookForm';
 
-// const bookList = {
-//   book1: "A Study in Scarlet & The Sign of Four",
-//   book2: "The Hound of the Baskervilles & The Valley of Fear",
-//   book3: "The Adventures of Sherlock Holmes"
-// }
-
 function App() {
 
-  const [bookList, setBookList] = useState(["A Study in Scarlet & The Sign of Four", "The Hound of the Baskervilles & The Valley of Fear", "The Adventures of Sherlock Holmes"]);
+  const [bookList, setBookList] = useState([]);
   const [newBook, updateNewBook] = useState('');
+
+  useEffect(() => {
+    // Fetch initial book list
+    fetchBookList();
+  }, []);
+
+  const fetchBookList = async () => {
+    try {
+      const response = await fetch('https://655e14659f1e1093c59a7c39.mockapi.io/shelf/list');
+      const data = await response.json();
+      setBookList(data.map(book => book.Title));
+    } catch (error) {
+      console.error('Error fetching book list:', error);
+    }
+  };
 
   const handleChange = (event) => {
     updateNewBook(event.target.value);
   }
 
-  const addBook = (event) => {
+  const addBook = async (event) => {
     event.preventDefault();
-    setBookList([...bookList, newBook]);
-    updateNewBook('');
+    
+    try {
+      // POST request to add a new book
+      await fetch('https://655e14659f1e1093c59a7c39.mockapi.io/shelf/list', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Title: newBook,
+          Cover: 'https://loremflickr.com/640/480',
+          url: 'http://example.com',
+        }),
+      });
+
+      // Fetch updated book list
+      fetchBookList();
+
+      // Clear the input
+      updateNewBook('');
+    } catch (error) {
+      console.error('Error adding book:', error);
+    }
   }
 
-  const removeBook = (index) => {
-    const updatedList = [...bookList];
-    updatedList.splice(index, 1);
-    setBookList(updatedList);
+  const removeBook = async (index) => {
+    try {
+      const response = await fetch(`https://655e14659f1e1093c59a7c39.mockapi.io/shelf/list/${index + 1}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        // Fetch updated book list
+        fetchBookList();
+      } else {
+        console.error('Error deleting book:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting book:', error);
+    }
   }
 
   return (
